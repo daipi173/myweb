eb14fbac9cbe7fc9724d7d74ec85a48527ee320f
daipi173 <939628886@qq.com>
2019-02-18 17:21:41 +0800

    my

--- daipi173-radio/js/index.js
+++ daipi173-radio/js/index.js
@@ -19,11 +19,11 @@ var timer,
     allWidth=232,
     timer2;
 var Img1 = document.getElementById('img1');
-// Audio.oncanplay = function(){
-//     StartTime.innerHTML = Change(Audio.currentTime);
-//     duration = this.duration;
-//     EndTime.innerHTML = Change(duration);
-// };
+Audio.oncanplay = function(){
+    StartTime.innerHTML = Change(Audio.currentTime);
+    duration = this.duration;
+    EndTime.innerHTML = Change(duration);
+};
 
 // 线上网络资源用ondurationchange
 // Audio.ondurationchange = function(){
@@ -31,11 +31,11 @@ var Img1 = document.getElementById('img1');
 //     duration = this.duration;
 //     EndTime.innerHTML = Change(duration);
 // };
-window.onload = function(){
-    StartTime.innerHTML = Change(Audio.currentTime);
-    duration = Audio.duration;
-    EndTime.innerHTML = Change(duration);
-};
+// window.onload = function(){
+//     StartTime.innerHTML = Change(Audio.currentTime);
+//     duration = Audio.duration;
+//     EndTime.innerHTML = Change(duration);
+// };
 
 //改变时间的格式
 function Change(time){
