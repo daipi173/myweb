f5a07497a014cbf433b3b29dddcbc4793f6151f1
daipi173 <939628886@qq.com>
2019-03-28 22:20:07 +0800

    vue

---
+++ daipi173-vue/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <my-header></my-header>
+    <my-body :title="descriptions"
+    :likes="liked"></my-body>
+    <my-footer :footers="footers"></my-footer>
+  </div>
+  
+</template>
+
+<script>
+import header from './components/header'
+import body  from './components/body'
+import descriptions from './data/saletext.js'
+import liked from "./data/like.js"
+import footer from './components/footer.vue'
+import footers from './data/footers.js'
+export default {
+  name: 'app',
+  components:{
+    'my-header': header,
+    'my-body' : body,
+    'my-footer': footer
+  },
+  data:function(){
+    return{
+      descriptions,
+      liked,
+      footers
+    }
+  }
+}
+</script>
+
+<style>
+  #app{
+    width: 750px;
+    height:108px;
+    margin: 0 auto;
+  }
+</style>
---
+++ daipi173-vue/src/data/footers.js
@@ -0,0 +1,23 @@
+export default[
+    {
+        footSrc: require ("../img/footer1.jpg"),
+        footDescription: "预定",
+        
+    },
+    {
+        footSrc: require ("../img/footer2.jpg"),
+        footDescription: "周边景点",
+    },
+    {
+        footSrc: require ("../img/24k.jpg"),
+        footDescription: "",
+    },
+    {
+        footSrc: require ("../img/footer3.jpg"),
+        footDescription: "票务",
+    },
+    {
+        footSrc: require ("../img/footer4.jpg"),
+        footDescription: "我的",
+    }
+]
